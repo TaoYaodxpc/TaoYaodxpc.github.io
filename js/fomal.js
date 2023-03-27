4722f21604fe12e742025dfecbe3b16cf65c49e1
TaoYaopc <dengcong0819@outlook.com>
2023-03-27 19:26:50 +0800

    Site updated: 2023-03-27 19:26:33

--- js/fomal.js
+++ js/fomal.js
@@ -73,7 +73,7 @@ $.ajax({
   type: 'get',
   url: 'https://apis.map.qq.com/ws/location/v1/ip',
   data: {
-    key: '',  // 这里要写你的KEY!!!
+    key: 'DSFBZ-TCZRU-FF2VZ-GFJLA-7UAIE-IZF3M',  // 这里要写你的KEY!!!
     output: 'jsonp',
   },
   dataType: 'jsonp',
